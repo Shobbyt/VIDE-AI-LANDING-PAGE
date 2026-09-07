5e8616ef20150f73275abda91e3386a7337318e6
Shobby Digital <shobbydigital07@gmail.com>
2026-09-07 15:50:59 +0100

    lastica

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
+import "./public/fonts/Lastica.ttf";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter basename="/VIDE-AI-LANDING-PAGE">
